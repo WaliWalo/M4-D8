4a5f1a4675cc3808e549678b57f4bbff2bf73d7e
Hung Jin Chong <hungjinchong@Outlook.com>
2020-11-25 14:58:17 +0000

    Done exercises

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import dishes from "../data/menu.json";
 import SingleDish from "./SingleDish.jsx";
-import { Container } from "react-bootstrap";
+import { Container, Alert } from "react-bootstrap";
 
-const Menu = () => (
-  <Container>
-    {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
-  </Container>
-);
+const Menu = () => {
+  //let dishes = [];
+  return (
+    <Container>
+      {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+      {dishes.length === 0 && (
+        <Alert variant="warning" className="mt-5 ">
+          No dish!!!
+        </Alert>
+      )}
+    </Container>
+  );
+};
+
 export default Menu;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,55 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link, withRouter } from 'react-router-dom'
+import { Link, withRouter } from "react-router-dom";
 
-const NavBar = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-        <Link to="/">
-          <Navbar.Brand>
-            {props.title} - Strive For Food
-          </Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Link to="/menu">
-              <div className={props.location.pathname === '/menu' ? 'nav-link active' : 'nav-link'}>Menu</div>
-            </Link>
-            <Link to="/reservation">
-              <div className={props.location.pathname === '/reservation' ? 'nav-link active' : 'nav-link'}>Reservation</div>
-            </Link>
-            <Nav.Link>Our Location</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
-};
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+  render() {
+    console.log("HELLO");
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Link to="/">
+            <Navbar.Brand>{this.props.title} - Strive For Food</Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              {this.props.location.pathname === "/details/2" && (
+                <div className="nav-link active"> MY FAVOURITE</div>
+              )}
+              <Link to="/menu">
+                <div
+                  className={
+                    this.props.location.pathname === "/menu"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Menu
+                </div>
+              </Link>
+              <Link to="/reservation">
+                <div
+                  className={
+                    this.props.location.pathname === "/reservation"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Reservation
+                </div>
+              </Link>
+              <Nav.Link>Our Location</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
-export default withRouter(NavBar); // will give the component exported EXTRA PROPS: history, location, match
+export default withRouter(NavBar); // will give the component exported EXTRA this.PROPS: history, location, match
